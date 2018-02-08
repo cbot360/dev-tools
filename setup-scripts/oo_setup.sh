accf13061fbab48f7b82d0dfb4d3b980786ca336
cbot360 <31928334+cbot360@users.noreply.github.com>
2018-02-08 18:52:04 +0530

    Update oo_setup.sh

--- setup-scripts/oo_setup.sh
+++ setup-scripts/oo_setup.sh
@@ -2,7 +2,7 @@
 
 export BUILD_BASE='/home/oneops/build'
 export OO_HOME='/home/oneops'
-export GITHUB_URL='https://github.com/oneops'
+export GITHUB_URL='https://github.com/cbot360'
 
 mkdir -p $BUILD_BASE
 
